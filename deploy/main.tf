6316576abb51e855f7fae8d7e80ded3f79232d63
lokeshjawane <lucky.cs025@gmail.com>
2020-11-11 18:48:04 +0530

    added tf for s3

---
+++ deploy/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "test" {
+  bucket = "bucket-solve-jobs"
+  acl    = "private"
+
+  tags = {
+    Name        = "bucket-solve-jobs"
+    Environment = "Dev"
+  }
+}
