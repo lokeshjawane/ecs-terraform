ffa7ec582f8797bedc2049107a4297bbb9a42ca3
lokeshjawane <lucky.cs025@gmail.com>
2020-11-11 19:17:58 +0530

    added tf state backend

--- deploy/main.tf
+++ deploy/main.tf
@@ -3,6 +3,15 @@ region = "us-east-1"
 }
 
 
+
+terraform {
+  backend "s3" {
+    bucket = "solvejobs-tfstates"
+    key    = "staging"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_s3_bucket" "test" {
   bucket = "bucket-solve-jobs"
   acl    = "private"
